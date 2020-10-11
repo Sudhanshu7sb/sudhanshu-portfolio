a1d8392d530283ec3ccc9f5840abeb8d0acfc80e
Sudhanshu7sb <sudhanshu7.sb@gmail.com>
2020-10-11 22:28:58 +0530

    Add anchor links

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,16 +4,16 @@ function Header() {
   return (
     <>
       <nav
-        class="navbar is-fixed-top"
+        class="navbar is-fixed-top has-background-grey-light"
         role="navigation"
         aria-label="main navigation"
       >
         <div class="navbar-brand">
-          <a class="navbar-item" href="https://bulma.io">
+          <a class="navbar-item mx-6" href="https://bulma.io">
             SUDHANSHU
           </a>
 
-          <a
+          <a href="#home"
             role="button"
             class="navbar-burger burger"
             aria-label="menu"
@@ -31,11 +31,11 @@ function Header() {
 
           <div class="navbar-end">
             <div class="navbar-item">
-              <a class="navbar-item mx-6">Home</a>
+              <a href="#home" class="navbar-item mx-6">Home</a>
 
-              <a class="navbar-item mx-6">Skills</a>
-              <a class="navbar-item mx-6">Projects</a>
-              <a class="navbar-item mx-6">Contact</a>
+              <a href="#skills"  class="navbar-item mx-6">Skills</a>
+              <a href="#projects" class="navbar-item mx-6">Projects</a>
+              <a href="#contact" class="navbar-item mx-6">Contact</a>
             </div>
           </div>
         </div>
@@ -45,47 +45,3 @@ function Header() {
 }
 
 export default Header;
-{
-  /* <nav
-        className="navbar container"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <a
-          role="button"
-          class="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a className="navbar-item" href="#home">
-              <h1 className="is-size-4">SUDHANSHU</h1>
-            </a>
-          </div>
-
-          <div className="navbar-end">
-            <a href="#home" className="navbar-item mx-5">
-              Home
-            </a>
-
-            <a href="#skills" className="navbar-item mx-5">
-              Skills
-            </a>
-            <a href="#projects" className="navbar-item mx-5">
-              Projects
-            </a>
-
-            <a href="#footer" className="navbar-item mx-5">
-              Contact
-            </a>
-          </div>
-        </div>
-      </nav> */
-}
