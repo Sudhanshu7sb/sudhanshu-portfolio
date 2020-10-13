dafb3624dd22a9251be9222da58dc658c0eb9b30
Sudhanshu7sb <sudhanshu7.sb@gmail.com>
2020-10-13 19:23:50 +0530

    changed function component to class component

--- src/component/Resume.jsx
+++ src/component/Resume.jsx
@@ -1,47 +1,67 @@
-import React,{ useState ,useRef} from "react";
+import React, { useRef } from "react";
 import Modal from "react-modal";
-import PDFViewer from 'pdf-viewer-reactjs' ;
+import PDFViewer from "pdf-viewer-reactjs";
 import ReactToPrint from "react-to-print";
 
+class Resume extends React.Component {
+  state = {
+    modalIsOpen: false,
+  };
+  // const [modalIsOpen, setmodalIsOpen] = useState(false);
 
-
-function Resume(){
-    const[modalIsOpen,setmodalIsOpen] = useState(false);
-
+  render() {
     return (
-        <>
-        <div className="has-text-centered resume-section pb-6" >
-            <button onClick={()=>{setmodalIsOpen(true)}} className="resume button is-info">View Resume</button>
-            <Modal isOpen={modalIsOpen} onRequestClose={() => {setmodalIsOpen(false)} }>
-            <div className="has-text-right">
-                   <button onClick={()=>{setmodalIsOpen(false)}}>X</button>
-               </div>
-            <PDFViewer
-            document={{
-                url: 'Sudhanshu-Shekhar-Behera-Resume.pdf',
+      <>
+        <div className="has-text-centered resume-section pb-6">
+          <button
+            onClick={() => {
+              this.setState({ modalIsOpen: true });
             }}
-            hideZoom={true} onPrevBtnClick={false}
-        />
-              
-            </Modal>
+            className="resume button is-info"
+          >
+            View Resume
+          </button>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onRequestClose={() => {
+              this.setState({ modalIsOpen: false });
+            }}
+          >
+            <div className="has-text-right">
+              <button
+                onClick={() => {
+                  this.setState({ modalIsOpen: false });
+                }}
+              >
+                X
+              </button>
+            </div>
+            <PDFViewer
+              document={{
+                url: "Sudhanshu-Shekhar-Behera-Resume.pdf",
+              }}
+              hideZoom={true}
+              onPrevBtnClick={false}
+            />
+          </Modal>
         </div>
-        </>
+      </>
     );
-
+  }
 }
 
 const Example = () => {
-    const componentRef = useRef();
-   
-    return (
-      <div>
-        <ReactToPrint
-          trigger={() => <button>Print this out!</button>}
-          content={() => componentRef.current}
-        />
-        <Resume ref={componentRef} />
-      </div>
-    );
-  };
-    
-  export default Example;
+  const componentRef = useRef();
+
+  return (
+    <div>
+      <ReactToPrint
+        trigger={() => <button>Print this out!</button>}
+        content={() => componentRef.current}
+      />
+      <Resume ref={componentRef} />
+    </div>
+  );
+};
+
+export default Example;
